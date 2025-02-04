4f68dfb6e5acca5caab5503585320e705347660f
d3lph1 <d3lph1.contact@gmail.com>
2025-02-05 02:48:09 +0400

    GitHub actions

--- crates/worldgen/benches/pprof.rs
+++ crates/worldgen/benches/pprof.rs
@@ -1,27 +1,27 @@
 use criterion::profiler::Profiler;
-use pprof::ProfilerGuard;
+// use pprof::ProfilerGuard;
 use std::fs::File;
 use std::os::raw::c_int;
 use std::path::Path;
 
-pub struct FlamegraphProfiler<'a> {
+pub struct FlamegraphProfiler {
     frequency: c_int,
-    active_profiler: Option<ProfilerGuard<'a>>,
+    // active_profiler: Option<ProfilerGuard<'a>>,
 }
 
-impl<'a> FlamegraphProfiler<'a> {
+impl FlamegraphProfiler {
     #[allow(dead_code)]
     pub fn new(frequency: c_int) -> Self {
         FlamegraphProfiler {
             frequency,
-            active_profiler: None,
+            // active_profiler: None,
         }
     }
 }
 
-impl<'a> Profiler for FlamegraphProfiler<'a> {
+impl Profiler for FlamegraphProfiler {
     fn start_profiling(&mut self, _benchmark_id: &str, _benchmark_dir: &Path) {
-        self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
+        // self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
     }
 
     fn stop_profiling(&mut self, _benchmark_id: &str, benchmark_dir: &Path) {
@@ -30,15 +30,15 @@ impl<'a> Profiler for FlamegraphProfiler<'a> {
         let flamegraph_path = benchmark_dir.join("flamegraph.svg");
         let flamegraph_file = File::create(&flamegraph_path)
             .expect("File system error while creating flamegraph.svg");
-        if let Some(profiler) = self.active_profiler.take() {
-            let report = profiler
-                .report()
-                .build()
-                .unwrap();
-
-            report
-                .flamegraph(flamegraph_file)
-                .expect("Error writing flamegraph");
-        }
+        // if let Some(profiler) = self.active_profiler.take() {
+        //     let report = profiler
+        //         .report()
+        //         .build()
+        //         .unwrap();
+        // 
+        //     report
+        //         .flamegraph(flamegraph_file)
+        //         .expect("Error writing flamegraph");
+        // }
     }
 }
